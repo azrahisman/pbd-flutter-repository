078cd5041732f0a8878de28fc643f2ddddd969a6
Azra Batrisyia Hisman <azrahisman@gmail.com>
2022-11-10 09:55:06 +0700

    added conditionals

--- counter_7/lib/main.dart
+++ counter_7/lib/main.dart
@@ -95,8 +95,8 @@ class _MyHomePageState extends State<MyHomePage> {
           // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+            Text(
+              _counter % 2 != 0 ? 'ODD' : 'EVEN',
             ),
             Text(
               '$_counter',
